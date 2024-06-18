a0d79a0ce33cdf2449751c8c3d2d3cf87ce5f270
devjuwon <devjuwonkuzoma@gmail.com>
2024-06-18 19:24:49 +0100

    javascript assignment

---
+++ app.js
@@ -0,0 +1,89 @@
+
+// javascript math methods
+let b = Math.abs(-7.25);
+let c = Math.abs(null);
+let d = Math.abs("Hello");
+
+console.log(b)
+
+Math.acos(0)
+console.log(Math.acos(0));
+
+let a = Math.ceil(0.60);
+console.log(a);
+
+let f = Math.ceil(-5.9);
+console.log(f);
+
+let e = Math.clz32(4);
+console.log(e);
+
+let x = Math.cos(3.14);
+console.log(x.toFixed(2));
+
+
+let j = Math.cosh()
+console.log(j);
+
+console.log(Math.E);
+
+console.log( Math.exp(3));
+console.log(Math.expm1(3));
+
+console.log(Math.floor(1.6));
+
+let q = Math.fround(2.60);
+console.log(q);
+
+console.log( Math.LN2);
+
+console.log(Math.log(2));
+
+let p = Math.log2(0);
+console.log(p);
+
+
+// javascript string methods
+
+// let text = "W3schools";
+// let character = text.at(0);
+// console.log(character);
+
+// let text = "HELLO WORLD";
+// let letter = text.charAt(0);
+// console.log(letter);
+
+// let text = "HELLO WORLD";
+// let code = text.charCodeAt(0);
+// console.log(code);
+
+// let text = "HELLO WORLD";
+// let code = text.codePointAt(0);
+// console.log(code);
+
+// let text1 = "Hello";
+// let text2 = "world!";
+// let result = text1.concat(" ", text2);
+// console.log(result);
+
+// let text = "Hello world";
+// let result =  text.endsWith("world");
+// console.log(result);
+
+// let text = String.fromCharCode(65);
+// console.log(text);
+
+// let text = "Hello world, welcome to the universe.";
+// let result = text.includes("world");
+// console.log(result);
+
+// let text = "a,b,c,d,e,f";
+// const myArray = text.split(",");
+// console.log(myArray[0]);
+
+// function myFunction(){
+// let text = "hello world!";
+// "hello world!"= text.toUpperCase()
+// }
+
+// console.log(myFunction());
